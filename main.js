b5e214b00749cac987bf445c5f5454e6c93d0aba
israel sandra <israelsandra2000@gmail.com>
2024-06-07 12:07:49 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// Show the preloader initially
+// document.querySelector("#preloader").style.display = "block";
+
+// Hide the preloader after a specified timeout (e.g., 1500 milliseconds)
+// window.onload = function() {
+//     setTimeout(() => {
+//       preloader.style.display = "none";
+//     }, 500);
+// };
+
+// const menu = document.querySelector(".menu");
+// const slideBox = document.querySelector(".slidebox");
+
+// menu.addEventListener("click", function(event){
+//     event.preventDefault();
+//     slideBox.style.display="block";
+// })
+// menu.addEventListener("click", function(event){
+//     event.preventDefault;
+//     slideBox.style.display="none";
+// })
+
+
