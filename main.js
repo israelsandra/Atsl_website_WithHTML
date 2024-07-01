f9156dbf3793bf459d5bc221f445c9569b2dce6b
israel sandra <israelsandra2000@gmail.com>
2024-07-01 09:07:27 +0100

    commit

--- main.js
+++ main.js
@@ -74,12 +74,12 @@ form.addEventListener('submit', function(e) {
 });
 
 
-function alpha(event) {
-    var char = event.which;
-    if (char >31 && char != 32 && (char<65 || char>90) && (char < 97 || char >122)){
-        return false;
-    }
-}
+// function alpha(event) {
+//     var char = event.which;
+//     if (char >31 && char != 32 && (char<65 || char>90) && (char < 97 || char >122)){
+//         return false;
+//     }
+// }
 // const submitBtn = document.querySelector(".btn")
 // const formBox = document.querySelector("#contactForm")
 
